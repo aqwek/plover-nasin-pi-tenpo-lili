8ee31e26e22c410875fe41ce7fd726a485badffe
aqwek <aqwekftw@gmail.com>
2026-03-11 18:29:15 +1000

    Initial commit

---
+++ plover_nasin_pi_tenpo_lili/system.py
@@ -0,0 +1,41 @@
+KEYS = (
+        'K-',
+        'E', '*',
+        '-N'
+    )
+
+STENO_ORDER = KEYS
+
+IMPLICIT_HYPHEN_KEYS = ('E') 
+
+NUMBER_KEY = None 
+NUMBERS = {}
+
+FERAL_NUMBER_KEY = False
+
+UNDO_STROKE_STENO = '*'
+
+SUFFIX_KEYS = ()
+ORTHOGRAPHY_RULES = []
+# ORTHOGRAPHY_RULES_ALIASES: Dict[str, str]
+# ORTHOGRAPHY_WORDLIST: Optional[str]
+
+KEYMAPS = {
+            "Gemini PR": {
+                },
+            "Keyboard": {
+                },
+            "Passport": {
+                },
+            "Plover HID": {
+                },
+            "Sentura": {
+                },
+            "TX Bolt": {
+                },
+            "Treal": {
+                },
+            }
+
+DICTIONARIES_ROOT = 'asset:plover_nasin_pi_tenpo_lili:dictionaries'
+DEFAULT_DICTIONARIES = ('nasin-pi-tenpo-lili-base.json',)
